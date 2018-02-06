199ef0da2cc87c6f5c360fd9fe122e8f39b03dc9
Michael Ouellette <michael.ouellette@industryweapon.com>
2018-02-06 12:41:09 -0500

    theme and basic layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { MichaelSidenav } from './common/sidenav';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MichaelSidenav
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/common/sidenav/index.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+/**
+ * @title sidenav
+ */
+@Component({
+      selector: 'michael-sidenav',
+      templateUrl: './sidenav.html',
+      styleUrls: ['sidenav.scss'],
+      encapsulation: ViewEncapsulation.None,
+      preserveWhitespaces: false,
+})
+export class MichaelSidenav { }

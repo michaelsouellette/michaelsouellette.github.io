b1f306af66d7c6cc463b71054b7f50dcca728b07
Michael Ouellette <michaelsouellette@gmail.com>
2018-02-07 00:35:01 -0500

    added flex-layout, sidenav language, and started a portfolio component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { MichaelSidenav } from './common/sidenav';
 
+// Components
+import { PortfolioComponent } from './components/portfolio';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MichaelSidenav
+    MichaelSidenav,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     AngularFontAwesomeModule,
     MaterialModule
   ],
---
+++ src/app/components/portfolio/index.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+/**
+ * @title sidenav
+ */
+@Component({
+      selector: 'portfolio-component',
+      templateUrl: './portfolio.html',
+      styleUrls: ['portfolio.scss'],
+      encapsulation: ViewEncapsulation.None,
+      preserveWhitespaces: false,
+})
+export class PortfolioComponent { }

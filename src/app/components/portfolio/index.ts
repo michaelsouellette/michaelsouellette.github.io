c505c2c832e4f6a0f1da719e61e05101b6733fe0
Michael Ouellette <michaelsouellette@gmail.com>
2018-02-25 23:07:10 -0500

    header text adjustment, and portfolio now utilizes slick carousel

--- src/app/components/portfolio/index.ts
+++ src/app/components/portfolio/index.ts
@@ -14,26 +14,22 @@ import { WordpressService } from '../../services/wordpress';
 export class PortfolioComponent implements OnInit {
       portfolioData = {};
       flexWidth = "32%";
+      slideConfig = {"slidesToShow": 3, "slidesToScroll": 1};
 
       constructor(private wordpress: WordpressService) { }
 
       ngOnInit(): void {
             this.wordpress.get('wp/v2/portfolio').then(data => {
                   console.log(data);
-                  for(let i = 0; i <= data.length; i++) {
+                  for(let i = 0; i < data['length']; i++) {
                         if(data[i]['featured_media']) {
-                              data[i].image = this.getImage(data[i]['featured_media'])
+                              console.log(data[i]['featured_media']);
+                              this.wordpress.media(data[i]['featured_media']).then(mediaData => {
+                                    data[i].image = mediaData;
+                              });
                         }
                   }
                   this.portfolioData = data;
-
-
-            });
-      }
-
-      getImage(id) {
-            this.wordpress.media(id).then(mediaData => {
-                  return mediaData['url'];
             });
       }
 }

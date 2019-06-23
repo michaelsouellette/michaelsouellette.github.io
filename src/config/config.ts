b9411f9470c884efe8eba89a82f06d03d5ec7e99
Michael Ouellette <michaelsouellette@gmail.com>
2019-06-22 20:12:54 -0400

    firebase variables and updated gitignore

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+      production: false,
+      firebase: {
+            apiKey: "AIzaSyB4qR7YwH6kIO-Dw-xs8-CWsHdbIWJLMH8",
+            authDomain: "raechulmae-6546a.firebaseapp.com",
+            databaseURL: "raechulmae-6546a.firebaseapp.com",
+            storageBucket: "raechulmae-6546a.appspot.com",
+            projectId: "raechulmae-6546a"
+      }
+};

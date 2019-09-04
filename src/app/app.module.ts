2de78e273131b4a11eca8656e254ef985ef54178
Michael Ouellette <michaelsouellette@gmail.com>
2019-09-03 20:05:14 -0400

    new angular configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { MaterialModule } from './material.module';
 
-// Libraries
-import { SlickModule } from 'ngx-slick';
-
-// Common
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MichaelSidenav } from './common/sidenav';
-
-// Components
-import { ContactComponent } from './components/contact';
-import { MainComponent } from './components/main';
-import { PortfolioComponent } from './components/portfolio';
-import { SkillsComponent } from './components/skills';
-
-// Services
-import { WordpressService } from './services/wordpress';
 
 @NgModule({
-      declarations: [
-            AppComponent,
-            MichaelSidenav,
-            ContactComponent,
-            MainComponent,
-            PortfolioComponent,
-            SkillsComponent
-      ],
-      imports: [
-            BrowserModule,
-            BrowserAnimationsModule,
-            HttpClientModule,
-            FlexLayoutModule,
-            AngularFontAwesomeModule,
-            MaterialModule,
-            SlickModule.forRoot()
-      ],
-      providers: [
-            WordpressService
-      ],
-      bootstrap: [
-            AppComponent
-      ]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }

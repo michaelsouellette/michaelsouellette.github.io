b9bd1aca80e73f117d1893ad6d6c5adc0f0f23e3
Michael Ouellette <michaelsouellette@gmail.com>
2020-04-05 23:36:38 -0400

    skills module built out

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SkillsInfo } from './skills.data';
+import { ISkill } from './skills.model';
+
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+	selector: 'app-skills',
+	templateUrl: './skills.component.html',
+	styleUrls: ['./skills.component.scss'],
+	providers: [SkillsInfo]
 })
 export class SkillsComponent implements OnInit {
+	public skills: ISkill[];
 
-  constructor() { }
+	constructor(
+		private skillsInfo: SkillsInfo
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.skills = this.skillsInfo.getSkillsInfo();
+	}
 
 }

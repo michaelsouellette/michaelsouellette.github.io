dd5c04e59ec9a4b48fcda1e3f862e7f53afc565c
Michael Ouellette <michaelsouellette@gmail.com>
2020-07-01 19:57:46 -0400

    lint fixes

--- src/app/features/resume/components/skills/skills.data.ts
+++ src/app/features/resume/components/skills/skills.data.ts
@@ -1,5 +1,5 @@
 import { IResumeSkills } from './skills.model';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ResumeSkills {
--- src/app/features/skills/skills.data.ts
+++ src/app/features/skills/skills.data.ts
@@ -1,5 +1,5 @@
 import { ISkill } from './skills.model';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SkillsInfo {

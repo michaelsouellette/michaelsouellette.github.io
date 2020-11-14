d2625990a6c33710242594443b4b1085b46dd7cb
Michael Ouellette <michaelsouellette@gmail.com>
2020-11-13 19:10:28 -0500

    angular 11 update

--- src/app/features/resume/components/resume-body/resume-body.component.spec.ts
+++ src/app/features/resume/components/resume-body/resume-body.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ResumeBodyComponent } from './resume-body.component';
 
@@ -6,7 +6,7 @@ describe('ResumeBodyComponent', () => {
 	let component: ResumeBodyComponent;
 	let fixture: ComponentFixture<ResumeBodyComponent>;
 
-	beforeEach(async(() => {
+	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
 			declarations: [ ResumeBodyComponent ]
 		})

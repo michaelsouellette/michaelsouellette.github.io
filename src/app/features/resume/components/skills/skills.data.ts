8871f6acb8b1d5abf37f23a65180d9ec9eb64a5e
Michael Ouellette <michaelsouellette@gmail.com>
2022-10-04 21:37:15 -0400

    Update resume and skills

--- src/app/features/resume/components/skills/skills.data.ts
+++ src/app/features/resume/components/skills/skills.data.ts
@@ -7,13 +7,14 @@ export class ResumeSkills {
 
 	constructor() {
 		this.skills.push(new IResumeSkills('Angular', 5));
+		this.skills.push(new IResumeSkills('TypeScript', 5));
+		this.skills.push(new IResumeSkills('React', 3));
 		this.skills.push(new IResumeSkills('Node.js', 4.5));
-		this.skills.push(new IResumeSkills('TypeScript', 4.5));
 		this.skills.push(new IResumeSkills('Express', 4));
 		this.skills.push(new IResumeSkills('MEAN', 4));
 		this.skills.push(new IResumeSkills('M*EAN', 4));
 		this.skills.push(new IResumeSkills('HTML 5', 5));
-		this.skills.push(new IResumeSkills('CSS 3', 5));
+		this.skills.push(new IResumeSkills('CSS', 5));
 		this.skills.push(new IResumeSkills('Sass', 5));
 		this.skills.push(new IResumeSkills('React', 3));
 		this.skills.push(new IResumeSkills('Javascript', 5));
@@ -22,23 +23,20 @@ export class ResumeSkills {
 		this.skills.push(new IResumeSkills('SQL', 4));
 		this.skills.push(new IResumeSkills('MySQL', 4));
 		this.skills.push(new IResumeSkills('NoSQL', 4));
-		this.skills.push(new IResumeSkills('Firebase', 3.5));
 		this.skills.push(new IResumeSkills('NPM', 4));
 		this.skills.push(new IResumeSkills('Bootstrap', 4.5));
 		this.skills.push(new IResumeSkills('Webpack', 3.5));
 		this.skills.push(new IResumeSkills('JSON', 5));
 		this.skills.push(new IResumeSkills('Python', 2));
 		this.skills.push(new IResumeSkills('PHP', 3));
-		this.skills.push(new IResumeSkills('Gulp', 3.5));
 		this.skills.push(new IResumeSkills('jQuery', 4));
 		this.skills.push(new IResumeSkills('WordPress', 3.5));
 		this.skills.push(new IResumeSkills('Google Cloud', 3.5));
 		this.skills.push(new IResumeSkills('AWS', 3.5));
-		this.skills.push(new IResumeSkills('Azure', 3.5));
+		this.skills.push(new IResumeSkills('Azure', 4));
 		this.skills.push(new IResumeSkills('Jira', 4));
 		this.skills.push(new IResumeSkills('XML', 4));
 		this.skills.push(new IResumeSkills('IBM WebSphere Commerce', 2));
-		this.skills.push(new IResumeSkills('Sketch', 3));
 		this.skills.push(new IResumeSkills('PhotoShop', 2));
 	}
 
